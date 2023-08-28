fb240ef30ec843828827dbb122d0d167d8985a0d
Syed Mufiz <syedmufiz935@gmail.com>
2023-08-28 08:48:44 +0530

    Updates

---
+++ precache-manifest.25d35d435156232dec33d5487d79dcf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c075f1358969e10c4096",
+    "url": "/TextUtils-React/static/css/main.82c468c6.chunk.css"
+  },
+  {
+    "revision": "c075f1358969e10c4096",
+    "url": "/TextUtils-React/static/js/main.54891fb1.chunk.js"
+  },
+  {
+    "revision": "bdb15c199812e17e16f8",
+    "url": "/TextUtils-React/static/js/runtime~main.c3f56c3a.js"
+  },
+  {
+    "revision": "95d99c7d9d392ce9b7c2",
+    "url": "/TextUtils-React/static/js/2.1e29dc99.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/TextUtils-React/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "c4fc9b3190f2fd167a6c12b261809661",
+    "url": "/TextUtils-React/index.html"
+  }
+];
